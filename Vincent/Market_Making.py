269980617a80ec5596ca79644545ec46ec3b9275
Vincent G <125322121+VincentG03@users.noreply.github.com>
2024-03-17 05:06:58 +1100

    MM changes
    Algorithm runs correctly.
    Currently producing a loss.
    Both sides of the quotes are not being executed, then we buy/sell to close which results in loss.

--- Vincent/Market_Making.py
+++ Vincent/Market_Making.py
@@ -11,6 +11,11 @@ Idea:
 - Check if outstanding positions:
 - YES: give the best order to the market to try to close out of that position
 - NO: market make both BID and ASK orders 
+
+
+SOMETHING TO ADD NOW:
+- only close out of a position if you made a profit or net zero. 
+- Will need to pass data to traderData. 
 """
 
 from datamodel import OrderDepth, UserId, TradingState, Order
@@ -28,8 +33,8 @@ class Trader:
             1.1 No outstanding positions --> Market make 
             1.2 Outstanding positions --> Send order to close position
         """
-        print("traderData: " + state.traderData)
-        print("Observations: " + str(state.observations))
+        #print("traderData: " + state.traderData)
+        #print("Observations: " + str(state.observations))
 
         #Set position limits 
         product_limits = {'AMETHYSTS': 15, 'STARFRUIT': 15}
@@ -40,13 +45,19 @@ class Trader:
         for product in state.order_depths:
             order_depth: OrderDepth = state.order_depths[product]
             orders: List[Order] = []
-            print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(len(order_depth.sell_orders)))
+            print(f"Buy Orders {product}: " + str(order_depth.buy_orders) + f", Sell Orders {product}: " + str(order_depth.sell_orders))
 
             #Find the position limit
             position_limit = self.find_position_limits(product, product_limits)
 
+            #Print current position
+            if len(state.position) != 0:
+                print(f"Current position is: {state.position}")
+            else:
+                print("No open positions")
+
             #(!!!) There are no open positions --> Send orders to attempt to MM
-            if product not in state.position or state.position.get(product) == 0:
+            if state.position.get(product, 0) == 0:
                 #Get the maximum amount of orders to send. There should be no outstanding positions so max is position limit set. 
                 qty_to_mm = abs(position_limit)
                
@@ -68,7 +79,6 @@ class Trader:
                     result[product] = orders
 
 
-
             #(!!!) There are open positions --> Send orders to close positions
             else:
                 qty_to_close = abs(state.position.get(product)) #Only continue if the asset has a current open position. 
@@ -76,12 +86,12 @@ class Trader:
                 if qty_to_close is not None: 
                     if state.position[product] > 0: #SELL to close                    
                         best_bid = list(order_depth.buy_orders.items())[0][0]
-                        print("SELL", str(qty_to_close) + "x", product, best_bid)
+                        print("(CLOSE) SELL", str(qty_to_close) + "x", product, best_bid)
                         orders.append(Order(product, best_bid, -qty_to_close))
                         
                     else: #BUY to close
                         best_ask = list(order_depth.sell_orders.items())[0][0]
-                        print("BUY", str(qty_to_close) + "x", product, best_ask)
+                        print("(CLOSE) BUY", str(qty_to_close) + "x", product, best_ask)
                         orders.append(Order(product, best_ask, qty_to_close))
 
                     result[product] = orders 
