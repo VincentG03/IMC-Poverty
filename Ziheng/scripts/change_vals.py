41a3c009fdac7e788519add1757404fc20bc8de5
zihengliao <zlia0050@student.monash.edu>
2024-04-01 00:27:49 +1100

    change_vals

--- Ziheng/scripts/change_vals.py
+++ Ziheng/scripts/change_vals.py
@@ -1,7 +1,7 @@
 
 def edit(file_name):
     # new file name
-    changed = "Ziheng/files/changed_file_sd"
+    changed = "Ziheng/files/changed_file"
     # var names:
     sd_multiplier = "sd_multiplier ="
     
@@ -23,36 +23,48 @@ def edit(file_name):
     avg_val = 2
 
     # ur gonna have to do some math to change it along side
-    for i in range(5, 15, 1):
+    for sd in range(5, 15, 1):       
         sd_checker = True
+        sd_new_val = sd /10
 
         for scale_factor in range(6, 12, 1):
             scale_factor = scale_factor / 10    
 
-            with open(file_name, "r") as f:
-                sd_new_val = i /10
+            for product_limit in range(20, 20, 2):
 
-                file_changed = changed + str(sd_new_val) + "sf" + str(scale_factor)
-                file_changed = file_changed.replace(".", "_")
-                file_changed = file_changed + ".py"
-                print(file_changed)
-                for line in f:
+                with open(file_name, "r") as f:
+                    
+                    # writing the file name
+                    file_changed = changed + "sd" + str(sd_new_val) + "sf" +\
+                          str(scale_factor) + "pl" + str(product_limit)
+                    
 
-                    # checks
-                    if sd_multiplier in line:
-                        sd_checker = False
-                        
+                    file_changed = file_changed.replace(".", "_")
+                    file_changed = file_changed + ".py"
+                    print(file_changed)
+
+                    for line in f:
+
+                        # checks
+                        if sd_multiplier in line:
+                            sd_checker = False
+                            
+                            with open(file_changed, "a") as c:
+                                c.write(f"            {sd_multiplier} {sd_new_val}")
+                            continue
+
+                        if scale_factor_dict in line:
+                            with open(file_changed, "a") as c:
+                                c.write(f"        {scale_factor_dict} {{'AMETHYSTS': {scale_factor}, 'STARFRUIT': {scale_factor}}} \n")
+                            continue
+
+                        if product_limits in line:
+                            with open(file_changed, "a") as c:
+                                c.write(f"        {product_limits} {{'AMETHYSTS': {product_limit}, 'STARFRUIT': {product_limit}}} \n")
+                            continue
+
                         with open(file_changed, "a") as c:
-                            c.write(f"            {sd_multiplier} {sd_new_val}")
-                        continue
-
-                    if scale_factor_dict in line:
-                        with open(file_changed, "a") as c:
-                            c.write(f"        {scale_factor_dict} {{'AMETHYSTS': {scale_factor}, 'STARFRUIT': {scale_factor}}} \n")
-                        continue
-
-                    with open(file_changed, "a") as c:
-                        c.write(line)
+                            c.write(line)
 
 
 
